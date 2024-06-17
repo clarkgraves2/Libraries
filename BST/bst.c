5962d75130b4f93783fd6aea3c09b8ec1aec1de4
clarkgraves2 <71040960+clarkgraves2@users.noreply.github.com>
2024-06-17 12:31:11 -0400

    Update bst.c

--- BST/bst.c
+++ BST/bst.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fnmatch.h>
+#include "Queue.h"
 
+struct node
+{
+  int data;  
+  node * left;
+  node * right;
+};
+
+struct bst
+{
+    node_t *front;
+    node_t *rear;
+    ssize_t size;
+};
