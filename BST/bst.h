1dd890d94e432795c8064b45a9bd248da52c574b
clark.i.e.graves64 <usacys@local>
2024-06-17 15:04:02 -0400

    End of Day Save

--- BST/bst.h
+++ BST/bst.h
@@ -5,5 +5,10 @@
 typedef struct bst bst_t;
 typedef struct node node_t;
 
+bst_t * create_new_tree(void);
+
+node_t * create_new_node(int data);
+
+node_t * insert_node(bst_t * bst, int data); 
 
 #endif
