3bb2701194a9596debf0790f9ba519baf8aa85e4
clark.i.e.graves64 <usacys@local>
2024-06-17 14:10:45 -0400

    Create Tree, Node Finished, Working on Insert_Node

--- BST/bst.c
+++ BST/bst.c
@@ -7,24 +7,43 @@
 struct node
 {
   int data;  
-  node * left;
-  node * right;
+  node_t * left;
+  node_t * right;
 };
 
 struct bst
 {
-   node * root;
+   node_t * root;
 };
 
-bst_t * create_new_tree(void)
+bst_t * 
+create_new_tree(void)
 {
-  bst_t * new_bst = calloc(1, sizeof(bst);
-  bst->root = NULL;
+  bst_t * new_bst = calloc(1, sizeof(bst_t));
+  new_bst->root = NULL;
   return new_bst;
 }
 
-
-node_t * create_new_node(node_t * root, int data)
+node_t * 
+create_new_node(int data) 
 {
-  
+    node_t* new_node = calloc(1,sizeof(node_t));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+node_t * 
+insert_node(bst_t * bst, int data) 
+{
+    if (NULL == bst->root)
+    {
+      create_new_node(data);
+    }
+    else if (data <= bst->root->data)
+    {
+      bst->root->left = insert_node(bst->root-)
+    }
+    return bst->root;
 }
--- BST/bst.h
+++ BST/bst.h
@@ -1,1 +1,9 @@
 
+#ifndef BST_H
+#define BST_H
+
+typedef struct bst bst_t;
+typedef struct node node_t;
+
+
+#endif
