e04ad4d08754c356806a4a298aba38607193177f
Clark Graves <clarkgraves2@gmail.com>
2024-07-10 07:32:04 -0400

    Pqueue Library Cleanup

--- Priority_Queue/priority_queue.c
+++ Priority_Queue/priority_queue.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 #define INITIAL_QUEUE_SIZE 8
+#define ROOT_INDEX 0
 
 typedef struct pqueue
 {
-    int **priority;    // Array of Priorities of items
+    int *priority;    // Array of Priorities of items
     void **item;       // Array of pointers
-    int size;        // Current size of the heap
+    int size_index;        // Current size of the heap 
     int capacity;    // Maximum capacity of the heap
 }pqueue_t;
 
@@ -20,9 +21,8 @@ pqueue_t * pqueue_create(void)
     {
         return NULL;
     }
-    base_p_queue->size = 0;
+    base_p_queue->size_index = 0;
     base_p_queue->capacity = INITIAL_QUEUE_SIZE;
-
     base_p_queue->item = calloc(INITIAL_QUEUE_SIZE, sizeof(void *));
     
     if (NULL == base_p_queue->item)
@@ -53,15 +53,7 @@ void pqueue_destroy(pqueue_t **pp_pqueue)
 
     pqueue_t *pqueue = *pp_pqueue;
 
-    for (int idx = 0; idx < pqueue->size; idx++)
-    {
-        free(pqueue->item[idx]);
-        free(pqueue->priority[idx]);
-        pqueue->item[idx] = NULL;
-        pqueue->priority[idx] = NULL;
-    }
-
-     free(pqueue->priority);
+    free(pqueue->priority);
     pqueue->priority = NULL;
 
     free(pqueue->item);
@@ -71,12 +63,6 @@ void pqueue_destroy(pqueue_t **pp_pqueue)
     *pp_pqueue = NULL;
 }
 
-// Insert Function: Implement a function to insert elements with priorities into the heap.
-// Extract Function: Implement a function to extract the highest priority element.
-// Heapify Functions: Implement heapify up and heapify down functions to maintain the heap property.
-// Resize Function: Implement a function to resize the array when the heap is full.
-
-
 bool pqueue_insert(pqueue_t * p_pqueue, int priority, void * data)
 {
     if (NULL == p_pqueue)
@@ -84,17 +70,149 @@ bool pqueue_insert(pqueue_t * p_pqueue, int priority, void * data)
         return false;
     }
 
+    if (p_pqueue->size_index == p_pqueue->capacity)
+    {
+        if (!pqueue_resize(p_pqueue))
+        {
+            return false;
+        }
+    }
 
+    p_pqueue->item[p_pqueue->size_index] = data;
+    p_pqueue->priority[p_pqueue->size_index] = priority; 
+    pqueue_heapify_up(p_pqueue, p_pqueue->size_index);
+    p_pqueue->size_index++;
+
+    return true; 
+}
+
+void *pqueue_extract(pqueue_t *pqueue)
+{
+    if (pqueue->size_index == 0)
+    {
+        return NULL; 
+    }
+
+  
+    void *root_item = pqueue->item[0];
+    int root_priority = pqueue->priority[0];
+
+   
+    pqueue->item[0] = pqueue->item[pqueue->size_index - 1];
+    pqueue->priority[0] = pqueue->priority[pqueue->size_index - 1];
+
+    pqueue->size_index--;
+
+    pqueue_heapify_down(pqueue, 0);
+
+    return root_item;
+}
+
+void *pqueue_peek(pqueue_t *p_pqueue)
+{
+    if (p_pqueue == NULL || p_pqueue->size_index == 0)
+    {
+        return NULL; 
+    }
+    return p_pqueue->item[0];
+}
+
+bool pqueue_is_empty(pqueue_t *p_pqueue)
+{
+    if (NULL == p_pqueue || 0 ==  p_pqueue->size_index)
+    {
+        return true;
+    }
+    return false;
+}
+
+int pqueue_size(pqueue_t * p_pqueue)
+{
+    if (NULL == p_pqueue)
+    {
+        return 0;
+    }
+    return p_pqueue->size_index;
+}
+
+static void pqueue_heapify_up(pqueue_t *pqueue, int current_index)
+{
+    while (current_index != ROOT_INDEX)
+    {
+        int parent_index = (current_index - 1) / 2;
+        if (pqueue->priority[current_index] >= pqueue->priority[parent_index])
+        {
+            break;
+        }
+
+        void *temp_item = pqueue->item[current_index];
+        pqueue->item[current_index] = pqueue->item[parent_index];
+        pqueue->item[parent_index] = temp_item;
+
+        int temp_priority = pqueue->priority[current_index];
+        pqueue->priority[current_index] = pqueue->priority[parent_index];
+        pqueue->priority[parent_index] = temp_priority;
+
+        current_index = parent_index;
+    }
+}
+
+static void pqueue_heapify_down(pqueue_t *pqueue, int index)
+{
+    while (index < pqueue->size_index)
+    {
+        int left_child = 2 * index + 1;
+        int right_child = 2 * index + 2;
+        int smallest = index;
+
+        if (left_child < pqueue->size_index && pqueue->priority[left_child] < pqueue->priority[smallest])
+        {
+            smallest = left_child;
+        }
+
+        if (right_child < pqueue->size_index && pqueue->priority[right_child] < pqueue->priority[smallest])
+        {
+            smallest = right_child;
+        }
+
+        if (smallest == index)
+        {
+            break;
+        }
+
+        void *temp_item = pqueue->item[index];
+        pqueue->item[index] = pqueue->item[smallest];
+        pqueue->item[smallest] = temp_item;
+
+        int temp_priority = pqueue->priority[index];
+        pqueue->priority[index] = pqueue->priority[smallest];
+        pqueue->priority[smallest] = temp_priority;
+
+        index = smallest;
+    }
 }
 
 
-// if p_weight is not NULL, pqueue_extract will store the weight
-// of the extracted data
-void *pqueue_extract(pqueue_t *p_pqueue, int *p_weight);
+ 
+static bool pqueue_resize(pqueue_t *pqueue)
+{
+    int new_capacity = pqueue->capacity * 2;
+    void **new_items = realloc(pqueue->item, new_capacity * sizeof(void *));
+    if (new_items == NULL)
+    {
+        return false; 
+    }
 
-// Maybe the same interface as extract?
-void *pqueue_peek(pqueue_t *p_pqueue);
+    int **new_priorities = realloc(pqueue->priority, new_capacity * sizeof(int *));
+    if (new_priorities == NULL)
+    {
+        free(new_items);
+        return false;
+    }
 
-bool pqueue_is_empty(pqueue_t *p_pqueue);
+    pqueue->item = new_items;
+    pqueue->priority = new_priorities;
+    pqueue->capacity = new_capacity;
 
-int pqueue_size(pqueue_t * p_pqueue);
+    return true;
+}
--- Priority_Queue/priority_queue.h
+++ Priority_Queue/priority_queue.h
@@ -7,26 +7,16 @@ typedef struct pqueue pqueue_t;
 
 pqueue_t * pqueue_create(void);
 
-// If del_f is not NULL, del_f() will be called on
-// each stored element that remains.
 void pqueue_destroy(pqueue_t **pp_pqueue);
 
 bool pqueue_insert(pqueue_t * p_pqueue, int weight, void * data);
 
-// if p_weight is not NULL, pqueue_extract will store the weight
-// of the extracted data
 void *pqueue_extract(pqueue_t *p_pqueue, int *p_weight);
 
-// Maybe the same interface as extract?
 void *pqueue_peek(pqueue_t *p_pqueue);
 
 bool pqueue_is_empty(pqueue_t *p_pqueue);
 
 int pqueue_size(pqueue_t * p_pqueue);
-
-// In .c file
-// static void bubble_up(pqueue_t * p_pqueue, int index);
-// static void bubble_down(pqueue_t * p_pqueue, int index);
-// static void resize(pqueue_t * p_pqueue);
 
 #endif
